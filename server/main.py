178e068c5d8131acbe25b62138250f10b9d720ae
mhabib <mustafa.habib@anu.edu.au>
2023-02-14 16:47:55 +1100

    uvicorn requirement for dev, also binding to localhost although its only to be used for dev - bandit

--- server/main.py
+++ server/main.py
@@ -14,4 +14,4 @@ app = FastAPI(**settings.fastapi_kwargs, dependencies=[Depends(lfdi_auth)])
 app.include_router(tm_router, tags=["time"])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
