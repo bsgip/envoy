9bc6efb73df6ad669e8a661e8bccbdeb26aac25f
mhabib <mustafa.habib@anu.edu.au>
2023-02-16 17:55:07 +1100

    replace pytz with dateutil, move time schema into right module, fix requirements

--- server/api/sep2/time.py
+++ server/api/sep2/time.py
@@ -1,11 +1,12 @@
 import datetime
+from zoneinfo import ZoneInfo
 
-import pytz
+from dateutil import tz
 from fastapi import APIRouter, Request
 from tzlocal import get_localzone
 
 from server.api.response import XmlResponse
-from server.schema.sep2.time import TimeResponse
+from server.schema.sep2.time import TimeQualityType, TimeResponse
 
 router = APIRouter()
 
@@ -41,15 +42,13 @@ async def get_time_resource(request: Request):
     random_non_dst_time = timezone.localize(datetime.datetime(2020, 6, 28, 15, 25))
     tz_offset = int(random_non_dst_time.utcoffset().total_seconds())
 
-    quality = 4
-
     time_dict = {
         "href": request.url.path,
         "currentTime": int(now_time.timestamp()),
         "dstEndTime": dst_info["dst_end"],
         "dstOffset": dst_info["dst_offset"],
         "dstStartTime": dst_info["dst_start"],
-        "quality": quality,
+        "quality": TimeQualityType.level_3_source,
         "tzOffset": tz_offset,
     }
 
@@ -66,55 +65,31 @@ def get_dst_info(now_time: datetime.datetime) -> dict:
         dst_info (dict):
 
     """
-    # Get timezone information
-    timezone_info = pytz.timezone(now_time.tzinfo.zone)
 
-    # Is timezone UTC? It's the default if a timezone can't be found. And it doesn't have the
-    # attribute _utc_transition_times, as it has no daylight savings.
-    if timezone_info in [pytz.UTC, pytz.utc, pytz.timezone("Etc/UTC")]:
-        dst_info = {"dst_end": 0, "dst_start": 0, "dst_offset": 0}
-        return dst_info
+    if now_time.tzinfo == ZoneInfo("UTC"):
+        return {"dst_end": 0, "dst_start": 0, "dst_offset": 0}
 
-    # Get the transition times in and out of daylight savings. These are in UTC
-    transition_times = timezone_info._utc_transition_times
-    transition_times.sort(reverse=False)
+    tzif_obj = tz.gettz(now_time.tzinfo._key)
 
-    # This is in UTC too
-    current_year = now_time.year
+    last_transition_idx = tzif_obj._find_last_transition(now_time)
+    if last_transition_idx >= (len(tzif_obj._trans_list_utc) - 1):
+        # No info about next transition - likely not a DST tz
+        return {"dst_end": 0, "dst_start": 0, "dst_offset": 0}
 
-    # Get the first daylight savings transitions for the current year. This would be the end of
-    # daylight savings in the southern hemisphere.
-    dst_end_time_index, dst_end_time = next(
-        ((i, t) for (i, t) in enumerate(transition_times) if t.year == current_year),
-        (None, None),
-    )
-
-    # Get the second daylight savings transition for the current year. This would be the start of
-    # daylight savings in the southern hemisphere
-    # Running the check again in case a jurisdiction decides to stop daylight savings
-    if dst_end_time is None:
-        dst_start_time = None
-        dst_offset = 0
+    # If we are in a DST period (i.e. dst offset total seconds > 0), then the last transition index is the start of the
+    # dst period and the next index is end.
+    dst_offset = now_time.dst().total_seconds()
+    if dst_offset > 0:
+        dst_start_time = tzif_obj._trans_list_utc[last_transition_idx]
+        dst_end_time = tzif_obj._trans_list_utc[last_transition_idx + 1]
     else:
-        dst_start_time_index = dst_end_time_index - 1
-        dst_start_time = transition_times[dst_start_time_index]
-
-        # Get the timezone offset. This should be usually be 1 hour
-        dst_offset = int(
-            timezone_info._transition_info[dst_start_time_index][1].total_seconds()
-        )
-
-        # Convert dst_start_time to unixtime
-        if dst_start_time is not None:
-            dst_start_time = int(dst_start_time.timestamp())
-
-        if dst_end_time is not None:
-            dst_end_time = int(dst_end_time.timestamp())
+        dst_start_time = tzif_obj._trans_list_utc[last_transition_idx + 1]
+        dst_end_time = tzif_obj._trans_list_utc[last_transition_idx + 2]
 
     # Put the start and end times in a dict to return
     dst_info = {
-        "dst_end": dst_end_time + now_time.utcoffset().seconds,
-        "dst_start": dst_start_time + now_time.utcoffset().seconds - dst_offset,
+        "dst_end": dst_end_time,
+        "dst_start": dst_start_time,
         "dst_offset": dst_offset,
     }
 
--- server/schema/sep2/base.py
+++ server/schema/sep2/base.py
@@ -40,32 +40,3 @@ class IdentifiedObject(Resource):
     description: Optional[str]
     mRID: mRIDType
     version: Optional[VersionType]
-
-
-""" Time
-"""
-
-
-# p170
-class TimeType(int):
-    # Unix time
-    pass
-
-
-# p170
-class TimeOffsetType(int):
-    # A sign time offset, typically applied to a TimeType value, expressed in seconds.
-    pass
-
-
-class TimeQualityType(enum.IntEnum):
-    authoritative_source = 3
-    level_3_source = 4
-    level_4_source = 5
-    level_5_source = 6
-    intentionally_uncoordinated = 7
-
-
-class DateTimeIntervalType(BaseXmlModelWithNS):
-    duration: int
-    start: TimeType
