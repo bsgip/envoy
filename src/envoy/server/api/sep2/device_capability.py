b8af67556ad1ba500814ed8328431f14f3794ee5
Mike Turner <mike.turner@anu.edu.au>
2023-03-28 08:13:35 +0800

    Added dcap api endpoint

---
+++ src/envoy/server/api/sep2/device_capability.py
@@ -0,0 +1,24 @@
+import logging
+
+from fastapi import APIRouter, Request
+
+from envoy.server.api.response import XmlResponse
+from envoy.server.schema.sep2.device_capability import DeviceCapabilityResponse
+
+router = APIRouter()
+
+logger = logging.getLogger(__name__)
+
+
+@router.head("/dcap")
+@router.get(
+    "/dcap",
+    response_class=XmlResponse,
+    response_model=DeviceCapabilityResponse,
+    status_code=200,
+)
+async def device_capability(request: Request):
+    logger.info("Handling device capability request")
+    dcap_dict = {"href": request.url.path}
+    dcap = DeviceCapabilityResponse(**dcap_dict)
+    return XmlResponse(dcap)
