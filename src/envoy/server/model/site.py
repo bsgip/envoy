9aebb6e63e207f0a7e88198c8755671743170271
Josh Vote <jjvote@gmail.com>
2023-03-29 18:55:02 +1100

    Added type hints

--- src/envoy/server/model/site.py
+++ src/envoy/server/model/site.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import VARCHAR, BigInteger, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -8,16 +10,16 @@ class Site(Base):
     __tablename__ = "site"
 
     site_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    nmi = mapped_column(VARCHAR(length=11), nullable=True)
-    aggregator_id = mapped_column(
+    nmi: Mapped[str] = mapped_column(VARCHAR(length=11), nullable=True)
+    aggregator_id: Mapped[int] = mapped_column(
         ForeignKey("aggregator.aggregator_id"), nullable=False
     )
     # dnsp_id = mapped_column(ForeignKey("dnsp.dnsp_id"), nullable=False) # TODO
 
-    changed_time = mapped_column(DateTime(timezone=True))
-    lfdi = mapped_column(VARCHAR(length=42), nullable=False, unique=True)
-    sfdi = mapped_column(BigInteger, nullable=False)
-    device_category = mapped_column(VARCHAR(length=8))
+    changed_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    lfdi: Mapped[str] = mapped_column(VARCHAR(length=42), nullable=False, unique=True)
+    sfdi: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    device_category: Mapped[str] = mapped_column(VARCHAR(length=8))
     # post_rate: Mapped[Integer] # TODO: should this live in notification/subscription tables?
 
     UniqueConstraint("sfdi", "aggregator_id", name="sfdi_aggregator_id_uc")
