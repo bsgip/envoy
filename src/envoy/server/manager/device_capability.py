3228fe7e51b013dee5c5ae778050b02e4b9baa1e
Mike Turner <mike.turner@anu.edu.au>
2023-04-14 13:08:37 +0800

    Added comment

--- src/envoy/server/manager/device_capability.py
+++ src/envoy/server/manager/device_capability.py
@@ -6,5 +6,7 @@ from envoy.server.schema.sep2.device_capability import DeviceCapabilityResponse
 class DeviceCapabilityManager:
     @staticmethod
     async def fetch_device_capability(aggregator_id: int) -> DeviceCapabilityResponse:
+        # Get all the 'Link's and 'ListLink's for a device capability response
         links = await link.get_supported_links(DeviceCapabilityResponse, aggregator_id)
+
         return DeviceCapabilityMapper.map_to_response(links)
