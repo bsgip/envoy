df4fee08f3e3ce5dcfb0bf1322223da302b4478c
Mike Turner <mike.turner@anu.edu.au>
2023-04-26 09:33:51 +0800

    Test now parses the request body and checks href

--- tests/integration/func_sets/test_device_capability.py
+++ tests/integration/func_sets/test_device_capability.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 import pytest
 from httpx import AsyncClient
 
+from envoy.server.schema.sep2.device_capability import DeviceCapabilityResponse
 from tests.integration.response import assert_response_header, read_response_body_string
 
 
@@ -18,3 +19,6 @@ async def test_get_device_capability(client: AsyncClient, uri: str, valid_header
     assert_response_header(response, HTTPStatus.OK)
     body = read_response_body_string(response)
     assert len(body) > 0
+
+    parsed_response: DeviceCapabilityResponse = DeviceCapabilityResponse.from_xml(body)
+    assert parsed_response.href == uri
