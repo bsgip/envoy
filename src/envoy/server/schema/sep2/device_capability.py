aadce5058ca5c1a6adf362735ba398eb27b82f4a
Mike Turner <mike.turner@anu.edu.au>
2023-05-05 04:50:28 +0800

    DeviceCapability no longer derives from FunctionSetAssignmentsBase

--- src/envoy/server/schema/sep2/device_capability.py
+++ src/envoy/server/schema/sep2/device_capability.py
@@ -3,10 +3,11 @@ from typing import Optional
 from pydantic_xml import attr, element
 
 from envoy.server.schema import uri
-from envoy.server.schema.sep2.base import DEFAULT_POLLRATE, FunctionSetAssignmentsBase, ListLink, PollRateType
+from envoy.server.schema.sep2.base import DEFAULT_POLLRATE, FunctionSetAssignmentsBase, ListLink, PollRateType, Resource
 
 
-class DeviceCapabilityResponse(FunctionSetAssignmentsBase):
+# class DeviceCapabilityResponse(FunctionSetAssignmentsBase):
+class DeviceCapabilityResponse(Resource):
     href: str = attr(default=uri.DeviceCapabilityUri)
     pollrate: PollRateType = DEFAULT_POLLRATE
 
