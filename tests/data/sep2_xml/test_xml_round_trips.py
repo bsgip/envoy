919179e59ea9f88420178a9ea1c5ad423572b34a
Mitchell Blyth <mitchelltblyth@gmail.com>
2023-05-08 11:28:55 +1000

    changes from PR review; changes for discussion with Mike re strict parameter, allows user to exclude known issues

--- tests/data/sep2_xml/test_xml_round_trips.py
+++ tests/data/sep2_xml/test_xml_round_trips.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable, List
+from typing import Callable
 from xml.etree import ElementTree as ET
 
 import pytest
@@ -12,23 +12,33 @@ INPUT_DIR = os.path.dirname(__file__)
 
 
 class AssertionResponse:
-    def __init__(self, input_type, value: bool, exceptions: List[str]) -> None:
+    def __init__(self, input_type, value: bool, exceptions: list[str]) -> None:
         self.input_type = input_type
         self.value = value
         self.exceptions = exceptions
 
 
-def comparison_func(ref: ET.Element, model: ET.Element, assertions: List[Callable]) -> AssertionResponse:
+def comparison_func(
+    ref: ET.Element,
+    model: ET.Element,
+    xml_type: str,
+    assertions: list[Callable],
+    allowed_missing: list[str],
+    strict: bool,
+) -> AssertionResponse:
     """Generic comparison function, which takes both a reference ET.Element and the
-    model ET.Element which we wish to compare against the reference, and a list of
-    assertions which should return True (and any optional extra return values) if
-    the assertions are True, or False (and any optional extra return values) if the
-    assertions are not True.
+    model ET.Element which we wish to compare against the reference, their xml_type as a
+    string, and a list of assertions which should return True (and any optional extra
+    return values) if the assertions are True, or False (and any optional extra
+    return values) if the assertions are not True.
 
     Returns an AssertionResponse which stores the name of the object against which
     comparison is being tested, and additionally stores True if all assertion funcs
     in the assertion list return True, else stores False alongside all the assertions
     and the extra information returned by each for each assertion which did not return True.
+
+    If strict is True, each Element referred to by name in allowed_missing list will
+    not be added to the exception list if missing.
 
     Returns ValueError if called on a ref and model possessing different top-level tags.
     """
@@ -38,7 +48,7 @@ def comparison_func(ref: ET.Element, model: ET.Element, assertions: List[Callabl
 
     exc = []
     for func in assertions:
-        ret_val, *extra = func(ref, model)
+        ret_val, *extra = func(ref, model, xml_type, allowed_missing, strict)
         if ret_val is not True:
             exc.append((func.__name__, extra))
 
@@ -46,8 +56,14 @@ def comparison_func(ref: ET.Element, model: ET.Element, assertions: List[Callabl
     return AssertionResponse(input_type=ref.tag, value=val, exceptions=exc)
 
 
-@pytest.mark.parametrize("file_name, xml_type, assertions_list", MAPPINGS)
-def test_xml_round_trip(file_name: str, xml_type: str, assertions_list: List[Callable]) -> None:
+@pytest.mark.parametrize("file_name, xml_type, assertions_list, allowed_missing, strict", MAPPINGS)
+def test_xml_round_trip(
+    file_name: str,
+    xml_type: str,
+    assertions_list: list[Callable],
+    allowed_missing: list[str],
+    strict: bool,
+) -> None:
     """Assert that we are able to round-trip examples from the 2030.5 spec document.
     Specifically, that we can instantiate an xml_type instance from that example, and
     that the XML generated from that instance passes some comparison_func metric against
@@ -65,6 +81,6 @@ def test_xml_round_trip(file_name: str, xml_type: str, assertions_list: List[Cal
     model_as_XML = ElementTree.fromstring(instance_as_bytes)
 
     comp: AssertionResponse
-    comp = comparison_func(ref_as_XML, model_as_XML, assertions_list)
+    comp = comparison_func(ref_as_XML, model_as_XML, xml_type, assertions_list, allowed_missing, strict)
 
     assert comp.value, (comp.input_type, comp.exceptions)
--- tests/data/sep2_xml/xml_to_model_mappings.py
+++ tests/data/sep2_xml/xml_to_model_mappings.py
@@ -12,8 +12,9 @@ from xml.etree import ElementTree as ET
 from envoy.server.schema.sep2.end_device import EndDeviceListResponse, EndDeviceResponse
 
 
-# example comparison function
-def compare_ET_Element_to_reference_ET_Element(ref: ET.Element, model: ET.Element) -> Tuple[bool, List[str]]:
+def compare_ET_Element_to_reference_ET_Element(
+    ref: ET.Element, model: ET.Element, xml_type: str, allowed_missing: list[str], strict: bool
+) -> Tuple[bool, List[str]]:
     """Compare an ElementTree.Element object against some reference object of the
     same type. We expect each element in ref to exist in model. The model is considered
     equivalent to ref iff:
@@ -34,16 +35,42 @@ def compare_ET_Element_to_reference_ET_Element(ref: ET.Element, model: ET.Elemen
     if model.attrib != ref.attrib:
         error_messages.append(f"Input attrib ({model.attrib}) does not match reference attrib ({ref.attrib})")
 
+    # reference inputs have trailing whitespace when read from buffer
+    # there's no 'right' way to deal with this in the absence of an XML schema
+    model_text = model.text.strip() if model.text is not None else ""
+    ref_text = ref.text.strip() if ref.text is not None else ""
+
+    if model_text != ref_text:
+        error_messages.append(f"Input text ({model.text}) does not match reference text ({ref.text})")
+
     # check if each element in the reference is present in the input model
-    for child in ref:
-        matches = [m for m in model if m.tag == child.tag]
-        if len(matches) != 1:
-            error_messages.append(f"Could not find matching child element for {child.tag} in reference.")
+    for c in ref:
+        matches = [m for m in model if m.tag == c.tag]
+
+        # Four conditions to consider, if there are no matches:
+        # 1. We are strict: error
+        # 2. Not strict and tag is required but we've explicitly allowed it to be missing: pass
+        # 3. Not strict and tag is required and we haven't allowed it to be missing: error
+        # 4. Not strict and tag is not required: pass
+
+        if not matches and strict:
+            error_messages.append(f"No matching child for {c.tag} in reference (strict = True).")
+
+        elif not matches and not strict and c.tag in xml_type.schema()["required"] and c.tag in allowed_missing:
+            pass
+
+        elif not matches and not strict and c.tag in xml_type.schema()["required"] and c.tag not in allowed_missing:
+            error_messages.append(f"No matching required child for {c.tag} in reference (tag required).")
+
+        elif not matches and not strict and c.tag not in xml_type.schema()["required"]:
+            pass
 
         else:
-            result, child_errors = compare_ET_Element_to_reference_ET_Element(child, matches[0])
+            result, child_errors = compare_ET_Element_to_reference_ET_Element(
+                c, matches[0], xml_type, allowed_missing, strict
+            )
             if not result:
-                error_messages.append(f"Child element ({child.tag}) failed comparison: {child_errors}")
+                error_messages.append(f"Child element ({c.tag}) failed comparison: {child_errors}")
 
     if error_messages:
         return False, error_messages
@@ -51,7 +78,6 @@ def compare_ET_Element_to_reference_ET_Element(ref: ET.Element, model: ET.Elemen
         return True, []
 
 
-# example list of assertions for EndDeviceListResponse instances
 enddevicelist_assertions = [
     compare_ET_Element_to_reference_ET_Element,
 ]
@@ -64,8 +90,8 @@ MAPPINGS = [
     # ("device_capability/devicecapability.xml", ..., ...),
     # ("does/dercontrollist.xml", ..., ...),
     # ("does/derprogramlist.xml", ..., ...),
-    ("end_device_resource/enddevicelist.xml", EndDeviceListResponse, enddevicelist_assertions),
-    ("end_device_resource/enddevice.xml", EndDeviceResponse, enddevice_assertions),
+    ("end_device_resource/enddevicelist.xml", EndDeviceListResponse, enddevicelist_assertions, [], False),
+    ("end_device_resource/enddevice.xml", EndDeviceResponse, enddevice_assertions, [], False),
     # ("end_device_resource/functionsetassignmentslist.xml", ..., ...),
     # ("end_device_resource/registration.xml", ..., ...),
     # ("meter_mirroring/meterreadinglist.xml", ..., ...),
