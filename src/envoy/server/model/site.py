7db4e1406bdeeb84bd262b9cd4b849e9e46ec78b
Josh Vote <jjvote@gmail.com>
2023-06-16 15:25:26 +1000

    Added admin site reading

--- src/envoy/server/model/site.py
+++ src/envoy/server/model/site.py
@@ -25,3 +25,24 @@ class Site(Base):
         UniqueConstraint("sfdi", "aggregator_id", name="sfdi_aggregator_id_uc"),
         UniqueConstraint("lfdi", "aggregator_id", name="lfdi_aggregator_id_uc"),  # Mirror Metering requires unique lfdi
     )
+
+
+# class SiteGroup(Base):
+#     """A way of logically grouping a site for a particular purpose"""
+
+#     __tablename__ = "site_group"
+
+#     site_group_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     name: Mapped[str] = mapped_column(VARCHAR(length=64), nullable=True)  # Human readable name of the group
+
+
+# class SiteGroupAssignment(Base):
+#     """Links a site to a group such that there can be a many-many relationship between the two"""
+
+#     __tablename__ = "site_group_assignment"
+
+#     site_group_assignment_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     site_group_id: Mapped[int] = mapped_column(ForeignKey("site_group.site_group_id"), nullable=False)
+#     site_id: Mapped[int] = mapped_column(ForeignKey("site.site_id"), nullable=False)
+
+#     __table_args__ = (UniqueConstraint("site_group_id", "site_id", name="site_group_id_site_id_uc"),)
