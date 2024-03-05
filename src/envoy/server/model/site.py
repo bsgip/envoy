6c6a65d8837ec469783aaffad6642be5b93a5c2e
Josh Vote <jjvote@gmail.com>
2024-03-05 17:42:11 +1100

    Basic models

--- src/envoy/server/model/site.py
+++ src/envoy/server/model/site.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 from typing import Optional
 
+from envoy_schema.server.schema.sep2.der import (
+    AbnormalCategoryType,
+    DERControlType,
+    DERType,
+    DOESupportedMode,
+    NormalCategoryType,
+)
 from envoy_schema.server.schema.sep2.types import DeviceCategory
 from sqlalchemy import INTEGER, VARCHAR, BigInteger, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -27,6 +34,13 @@ class Site(Base):
         cascade="all, delete",
         passive_deletes=True,
     )  # What assignments reference this group
+
+    site_ders: Mapped[list["SiteDER"]] = relationship(
+        back_populates="site",
+        lazy="raise",
+        cascade="all, delete",
+        passive_deletes=True,
+    )  # What DER live underneath/behind this site
 
     __table_args__ = (
         UniqueConstraint("sfdi", "aggregator_id", name="sfdi_aggregator_id_uc"),
@@ -71,3 +85,150 @@ class SiteGroupAssignment(Base):
 
     # We don't want a single site to be linked to a group multiple times
     __table_args__ = (UniqueConstraint("site_id", "site_group_id", name="site_id_site_group_id_uc"),)
+
+
+class SiteDER(Base):
+    """Represents a Distributed Energy Resource behind a Site's connection point - primarily a repository
+    of metadata / ratings"""
+
+    __tablename__ = "site_der"
+
+    site_der_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    site_id: Mapped[int] = mapped_column(ForeignKey("site.site_id", ondelete="CASCADE"))
+
+    changed_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
+
+    site: Mapped["Site"] = relationship(back_populates="site_ders", lazy="raise")
+    site_der_rating: Mapped[Optional["SiteDERRating"]] = relationship(
+        back_populates="site_der", uselist=False, lazy="raise"
+    )
+    site_der_setting: Mapped[Optional["SiteDERSetting"]] = relationship(
+        back_populates="site_der", uselist=False, lazy="raise"
+    )
+
+
+class SiteDERRating(Base):
+    """Represents the nameplate rating values associated with a SiteDER. These are not expected to change
+    after initially being set (excepting erroneous assignments). Only a single SiteDERRating should be assigned
+    to a SiteDER"""
+
+    __tablename__ = "site_der_rating"
+
+    site_der_rating_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    site_der_id: Mapped[int] = mapped_column(ForeignKey("site_der.site_der_id", ondelete="CASCADE"))
+    changed_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
+
+    # These values correspond to a flattened version of sep2 DERCapability
+    modes_supported: Mapped[DERControlType] = mapped_column(INTEGER, nullable=True)
+    abnormal_category: Mapped[Optional[AbnormalCategoryType]] = mapped_column(INTEGER, nullable=True)
+    max_a_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_a_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_ah_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_ah_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_va_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_va_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_w_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_w_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_va_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_va_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_w_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_w_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_v_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_v_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_va_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_va_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_neg_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_neg_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_w_value: Mapped[int] = mapped_column(INTEGER)
+    max_w_multiplier: Mapped[int] = mapped_column(INTEGER)
+    max_wh_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_wh_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_over_excited_displacement: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_over_excited_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_under_excited_displacement: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_under_excited_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_v_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_v_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    normal_category: Mapped[Optional[NormalCategoryType]] = mapped_column(INTEGER, nullable=True)
+    over_excited_pf_displacement: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    over_excited_pf_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    over_excited_w_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    over_excited_w_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    reactive_susceptance_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    reactive_susceptance_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    under_excited_pf_displacement: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    under_excited_pf_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    under_excited_w_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    under_excited_w_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_nom_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_nom_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    der_type: Mapped[DERType] = mapped_column(INTEGER)
+    doe_modes_supported: Mapped[Optional[DOESupportedMode]] = mapped_column(INTEGER, nullable=True)
+
+    site_der: Mapped["SiteDER"] = relationship(back_populates="site_der_rating", lazy="raise", single_parent=True)
+    __table_args__ = (UniqueConstraint("site_der_id"),)  # Only one SiteDERRating allowed per SiteDER)
+
+
+class SiteDERSetting(Base):
+    """Represents the current setting values associated with a SiteDER. The SiteDERRating represents
+    the ratings/limits while the settings represent the currently enabled functionality"""
+
+    __tablename__ = "site_der_setting"
+
+    site_der_setting_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    site_der_id: Mapped[int] = mapped_column(ForeignKey("site_der.site_der_id", ondelete="CASCADE"))
+    changed_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
+
+    # These values correspond to a flattened version of sep2 DERSettings
+    modes_enabled: Mapped[DERControlType] = mapped_column(INTEGER, nullable=True)
+    es_delay: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_high_freq: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_high_volt: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_low_freq: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_low_volt: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_ramp_tms: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_random_delay: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    es_grad_w: Mapped[int] = mapped_column(INTEGER)
+    max_a_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_a_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_ah_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_ah_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_va_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_va_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_w_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_charge_rate_w_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_va_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_va_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_w_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_discharge_rate_w_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_v_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_v_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_va_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_va_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_neg_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_var_neg_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_w_value: Mapped[int] = mapped_column(INTEGER)
+    max_w_multiplier: Mapped[int] = mapped_column(INTEGER)
+    max_wh_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    max_wh_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_over_excited_displacement: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_over_excited_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_under_excited_displacement: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_pf_under_excited_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_v_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    min_v_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    soft_grad_w: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_nom_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_nom_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_ref_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_ref_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_ref_ofs_value: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    v_ref_ofs_multiplier: Mapped[Optional[int]] = mapped_column(INTEGER, nullable=True)
+    doe_modes_enabled: Mapped[Optional[DOESupportedMode]] = mapped_column(INTEGER, nullable=True)
+
+    site_der: Mapped["SiteDER"] = relationship(back_populates="site_der_setting", lazy="raise", single_parent=True)
+    __table_args__ = (UniqueConstraint("site_der_id"),)  # Only one SiteDERSetting allowed per SiteDER)
