4720c251a2f86e640b0c1b5c713470aa11ba1c05
Josh Vote <jjvote@gmail.com>
2024-03-11 22:37:11 +1100

    More manager tests

--- src/envoy/server/manager/der.py
+++ src/envoy/server/manager/der.py
@@ -46,13 +46,13 @@ async def site_der_for_site(session: AsyncSession, aggregator_id: int, site_id: 
     default site der.
 
     Raises NotFoundError if site_id is missing / not accessible"""
-    site_der = await select_site_der_for_site(session=session, site_id=site_id, aggregator_id=aggregator_id)
+    site_der = await select_site_der_for_site(session, site_id=site_id, aggregator_id=aggregator_id)
     if site_der is None:
         # Validate the site exists / is accessible first
-        site = await select_single_site_with_site_id(session, site_id, aggregator_id)
+        site = await select_single_site_with_site_id(session, site_id=site_id, aggregator_id=aggregator_id)
         if site is None:
             raise NotFoundError(f"site with id {site_id} not found")
-        site_der = generate_default_site_der(site_id, site.changed_time)
+        site_der = generate_default_site_der(site_id=site_id, changed_time=site.changed_time)
 
     return site_der
 
@@ -70,7 +70,7 @@ class DERManager:
         """Provides a list view of DER for a specific site. Raises NotFoundError if DER/Site couldn't be accessed"""
 
         # If there isn't custom DER info already in place - return a default
-        site_der = await site_der_for_site(session, request_params.aggregator_id, site_id)
+        site_der = await site_der_for_site(session, aggregator_id=request_params.aggregator_id, site_id=site_id)
         site_der.site_der_id = PUBLIC_SITE_DER_ID
 
         # Manually filter - we are forcing our single DER into a simple list
@@ -97,11 +97,11 @@ class DERManager:
     ) -> DER:
         """Fetches a single DER for a specific site. Raises NotFoundError if DER/Site couldn't be accessed"""
 
-        site_der = await site_der_for_site(session, request_params.aggregator_id, site_id)
-        site_der.site_der_id = PUBLIC_SITE_DER_ID
-
         if site_der_id != PUBLIC_SITE_DER_ID:
             raise NotFoundError(f"no DER with id {site_der_id} in site {site_id}")
+
+        site_der = await site_der_for_site(session, aggregator_id=request_params.aggregator_id, site_id=site_id)
+        site_der.site_der_id = PUBLIC_SITE_DER_ID
 
         return DERMapper.map_to_response(request_params, site_der, DOE_PROGRAM_ID)
 
@@ -118,7 +118,7 @@ class DERCapabilityManager:
         """Fetches a single DER Capability for a specific DER. Raises NotFoundError if DER/Site couldn't be accessed
         or if no DERCapability has been stored."""
 
-        site_der = await site_der_for_site(session, request_params.aggregator_id, site_id)
+        site_der = await site_der_for_site(session, aggregator_id=request_params.aggregator_id, site_id=site_id)
         site_der.site_der_id = PUBLIC_SITE_DER_ID
 
         if site_der_id != PUBLIC_SITE_DER_ID:
@@ -146,7 +146,7 @@ class DERCapabilityManager:
         now = utc_now()
         new_der_rating = DERCapabilityMapper.map_from_request(now, der_capability)
 
-        site_der = await site_der_for_site(session, request_params.aggregator_id, site_id)
+        site_der = await site_der_for_site(session, aggregator_id=request_params.aggregator_id, site_id=site_id)
         if site_der.site_der_id is None:
             # we are inserting a whole new DER and rating
             site_der.site_der_rating = new_der_rating
--- tests/unit/server/manager/test_der.py
+++ tests/unit/server/manager/test_der.py
@@ -1,19 +1,190 @@
-from datetime import datetime, timezone
+import unittest.mock as mock
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 import pytest
-from envoy_schema.server.schema.sep2.der import DERAvailability, DERCapability, DERControlType, DERSettings, DERStatus
+from envoy_schema.server.schema.sep2.der import (
+    DERAvailability,
+    DERCapability,
+    DERControlType,
+    DERListResponse,
+    DERSettings,
+    DERStatus,
+)
 from sqlalchemy import func, select
 
 from envoy.server.crud.der import generate_default_site_der, select_site_der_for_site
 from envoy.server.exception import NotFoundError
-from envoy.server.manager.der import PUBLIC_SITE_DER_ID, DERCapabilityManager
+from envoy.server.manager.der import PUBLIC_SITE_DER_ID, DERCapabilityManager, DERManager, site_der_for_site
+from envoy.server.mapper.csip_aus.doe import DOE_PROGRAM_ID
 from envoy.server.mapper.sep2.der import DERCapabilityMapper, to_hex_binary
-from envoy.server.model.site import SiteDER, SiteDERAvailability, SiteDERRating, SiteDERSetting, SiteDERStatus
+from envoy.server.model.site import Site, SiteDER, SiteDERAvailability, SiteDERRating, SiteDERSetting, SiteDERStatus
 from envoy.server.request_state import RequestStateParameters
 from tests.assert_time import assert_datetime_equal
 from tests.data.fake.generator import assert_class_instance_equality, clone_class_instance, generate_class_instance
 from tests.postgres_testing import generate_async_session
+from tests.unit.mocks import assert_mock_session, create_mock_session
+
+
+@mock.patch("envoy.server.manager.der.select_site_der_for_site")
+@mock.patch("envoy.server.manager.der.select_single_site_with_site_id")
+@mock.patch("envoy.server.manager.der.generate_default_site_der")
+@pytest.mark.anyio
+async def test_site_der_for_site_no_der(
+    mock_generate_default_site_der: mock.MagicMock,
+    mock_select_single_site_with_site_id: mock.MagicMock,
+    mock_select_site_der_for_site: mock.MagicMock,
+):
+    """Fetch when no existing SiteDER exists"""
+    site_id = 123
+    agg_id = 456
+    mock_session = create_mock_session()
+
+    site_der: SiteDER = generate_class_instance(SiteDER)
+    site: Site = generate_class_instance(Site)
+    mock_select_site_der_for_site.return_value = None
+    mock_select_single_site_with_site_id.return_value = site
+    mock_generate_default_site_der.return_value = site_der
+
+    result = await site_der_for_site(mock_session, agg_id, site_id)
+    assert result is site_der
+
+    mock_select_site_der_for_site.assert_called_once_with(mock_session, site_id=site_id, aggregator_id=agg_id)
+    mock_select_single_site_with_site_id.assert_called_with(mock_session, site_id=site_id, aggregator_id=agg_id)
+    mock_generate_default_site_der.assert_called_with(site_id=site_id, changed_time=site.changed_time)
+    assert_mock_session(mock_session)
+
+
+@mock.patch("envoy.server.manager.der.select_site_der_for_site")
+@mock.patch("envoy.server.manager.der.select_single_site_with_site_id")
+@mock.patch("envoy.server.manager.der.generate_default_site_der")
+@pytest.mark.anyio
+async def test_site_der_for_site_existing_der(
+    mock_generate_default_site_der: mock.MagicMock,
+    mock_select_single_site_with_site_id: mock.MagicMock,
+    mock_select_site_der_for_site: mock.MagicMock,
+):
+    """Fetch when SiteDER already exists"""
+    site_id = 123
+    agg_id = 456
+    mock_session = create_mock_session()
+
+    site_der: SiteDER = generate_class_instance(SiteDER)
+    mock_select_site_der_for_site.return_value = site_der
+
+    result = await site_der_for_site(mock_session, agg_id, site_id)
+    assert result is site_der
+
+    mock_select_site_der_for_site.assert_called_once_with(mock_session, site_id=site_id, aggregator_id=agg_id)
+    mock_select_single_site_with_site_id.assert_not_called()
+    mock_generate_default_site_der.assert_not_called()
+    assert_mock_session(mock_session)
+
+
+@mock.patch("envoy.server.manager.der.select_site_der_for_site")
+@mock.patch("envoy.server.manager.der.select_single_site_with_site_id")
+@mock.patch("envoy.server.manager.der.generate_default_site_der")
+@pytest.mark.anyio
+async def test_site_der_for_site_inaccessible_site(
+    mock_generate_default_site_der: mock.MagicMock,
+    mock_select_single_site_with_site_id: mock.MagicMock,
+    mock_select_site_der_for_site: mock.MagicMock,
+):
+    """Fetch when SiteDER doesn't exist and the site isn't accessible"""
+    site_id = 123
+    agg_id = 456
+    mock_session = create_mock_session()
+
+    mock_select_site_der_for_site.return_value = None
+    mock_select_single_site_with_site_id.return_value = None
+
+    with pytest.raises(NotFoundError):
+        await site_der_for_site(mock_session, agg_id, site_id)
+
+    mock_select_site_der_for_site.assert_called_once_with(mock_session, site_id=site_id, aggregator_id=agg_id)
+    mock_select_single_site_with_site_id.assert_called_with(mock_session, site_id=site_id, aggregator_id=agg_id)
+    mock_generate_default_site_der.assert_not_called()
+    assert_mock_session(mock_session)
+
+
+@mock.patch("envoy.server.manager.der.DERMapper")
+@mock.patch("envoy.server.manager.der.site_der_for_site")
+@pytest.mark.anyio
+async def test_fetch_der_for_site_der_exists(
+    mock_site_der_for_site: mock.MagicMock,
+    mock_DERMapper: mock.MagicMock,
+):
+    """Fetch when site_der_for_site returns an instance"""
+    site_id = 123
+    rs_params = RequestStateParameters(456, None)
+    mock_session = create_mock_session()
+
+    site_der: SiteDER = generate_class_instance(SiteDER, seed=101)
+    mock_site_der_for_site.return_value = site_der
+    mock_map = mock.Mock()
+    mock_DERMapper.map_to_response = mock.Mock(return_value=mock_map)
+
+    result = await DERManager.fetch_der_for_site(mock_session, site_id, PUBLIC_SITE_DER_ID, rs_params)
+    assert result is mock_map
+
+    assert site_der.site_der_id == PUBLIC_SITE_DER_ID, "This should've been set during the fetch"
+    mock_DERMapper.map_to_response.assert_called_once_with(rs_params, site_der, DOE_PROGRAM_ID)
+    mock_site_der_for_site.assert_called_once_with(mock_session, aggregator_id=rs_params.aggregator_id, site_id=site_id)
+    assert_mock_session(mock_session)
+
+
+@mock.patch("envoy.server.manager.der.DERMapper")
+@mock.patch("envoy.server.manager.der.site_der_for_site")
+@pytest.mark.anyio
+async def test_fetch_der_for_site_bad_der_id(
+    mock_site_der_for_site: mock.MagicMock,
+    mock_DERMapper: mock.MagicMock,
+):
+    """Fetch when DER ID is incorrect"""
+    site_id = 123
+    rs_params = RequestStateParameters(456, None)
+    mock_session = create_mock_session()
+
+    with pytest.raises(NotFoundError):
+        await DERManager.fetch_der_for_site(mock_session, site_id, PUBLIC_SITE_DER_ID + 1, rs_params)
+
+    mock_DERMapper.assert_not_called()
+    mock_site_der_for_site.assert_not_called()
+    assert_mock_session(mock_session)
+
+
+AFTER_EPOCH = datetime(2022, 10, 9, 8, 7, 6, tzinfo=timezone.utc)
+
+
+@mock.patch("envoy.server.manager.der.site_der_for_site")
+@pytest.mark.parametrize(
+    "start, limit, after, expected_count",
+    [
+        (0, 99, AFTER_EPOCH - timedelta(seconds=10), 1),
+        (0, 0, AFTER_EPOCH - timedelta(seconds=10), 0),
+        (1, 99, AFTER_EPOCH - timedelta(seconds=10), 0),
+        (0, 99, AFTER_EPOCH + timedelta(seconds=10), 0),
+    ],
+)
+@pytest.mark.anyio
+async def test_fetch_der_list_for_site_pagination(
+    mock_site_der_for_site: mock.MagicMock, start: int, limit: int, after: datetime, expected_count: int
+):
+    """Fetch when site_der_for_site returns an instance"""
+    site_id = 123
+    rs_params = RequestStateParameters(456, None)
+    mock_session = create_mock_session()
+
+    site_der: SiteDER = generate_class_instance(SiteDER, seed=101)
+    site_der.changed_time = AFTER_EPOCH
+    mock_site_der_for_site.return_value = site_der
+
+    result = await DERManager.fetch_der_list_for_site(mock_session, site_id, rs_params, start, limit, after)
+    assert isinstance(result, DERListResponse)
+
+    assert len(result.DER_) == expected_count
+    mock_site_der_for_site.assert_called_once_with(mock_session, aggregator_id=rs_params.aggregator_id, site_id=site_id)
+    assert_mock_session(mock_session)
 
 
 @pytest.mark.parametrize(
