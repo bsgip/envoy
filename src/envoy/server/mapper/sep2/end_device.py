34634fcb9b1ff6cdac9510f7848ba45fe04f445e
Mike Turner <mike.turner@anu.edu.au>
2024-03-21 11:48:07 +1100

    EndDeviceMapper now uses parse_device_category

--- src/envoy/server/mapper/sep2/end_device.py
+++ src/envoy/server/mapper/sep2/end_device.py
@@ -4,10 +4,10 @@ from typing import Sequence
 import envoy_schema.server.schema.uri as uri
 from envoy_schema.server.schema.csip_aus.connection_point import ConnectionPointLink
 from envoy_schema.server.schema.sep2.end_device import EndDeviceListResponse, EndDeviceRequest, EndDeviceResponse
-from envoy_schema.server.schema.sep2.types import DEVICE_CATEGORY_ALL_SET, DeviceCategory, SubscribableType
 from envoy_schema.server.schema.sep2.identification import ListLink
-from envoy.server.exception import InvalidMappingError
-from envoy.server.mapper.common import generate_href
+from envoy_schema.server.schema.sep2.types import SubscribableType
+
+from envoy.server.mapper.common import generate_href, parse_device_category
 from envoy.server.model.site import Site
 from envoy.server.request_state import RequestStateParameters
 from envoy.server.settings import settings
@@ -34,24 +34,12 @@ class EndDeviceMapper:
 
     @staticmethod
     def map_from_request(end_device: EndDeviceRequest, aggregator_id: int, changed_time: datetime) -> Site:
-        # deviceCategory is a hex string
-        device_category: DeviceCategory
-        if end_device.deviceCategory:
-            raw_dc = int(end_device.deviceCategory, 16)
-            if raw_dc > DEVICE_CATEGORY_ALL_SET or raw_dc < 0:
-                raise InvalidMappingError(
-                    f"deviceCategory: {end_device.deviceCategory} int({raw_dc}) doesn't map to a known DeviceCategory"
-                )
-            device_category = DeviceCategory(raw_dc)
-        else:
-            device_category = DeviceCategory(0)
-
         return Site(
             lfdi=end_device.lFDI,
             sfdi=end_device.sFDI,
             changed_time=changed_time,
             aggregator_id=aggregator_id,
-            device_category=device_category,
+            device_category=parse_device_category(end_device.deviceCategory),
             timezone_id=settings.default_timezone,
         )
 
