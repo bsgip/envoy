c105181a62ac8d8452a98ad6bff46fbbcb4e2df8
Mustafa Habib <mustafa.habib@anu.edu.au>
2024-06-11 13:28:54 +1000

    tidy

--- tests/unit/server/crud/test_auth.py
+++ tests/unit/server/crud/test_auth.py
@@ -7,10 +7,10 @@ from tests.assert_type import assert_list_type
 
 @pytest.mark.anyio
 async def test_select_all_client_id_details(pg_base_config):
-    """Tests that select_all_client_id_details behaves with the base config"""
+    """Tests that select_all_client_id_details behaves with the base config.
+    Base config has 4 active certificates registered."""
     async with generate_async_session(pg_base_config) as session:
         # Test the basic config is there and accessible
         result = await select_all_client_id_details(session)
 
-    print(result)
     assert_list_type(ClientIdDetails, result, count=4)
