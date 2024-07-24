97d10122f491548d4bb1690cd3e9bd6a8b8d04d3
Mustafa Habib <mustafa.habib@anu.edu.au>
2024-07-24 16:05:45 +1000

    modify content-length header

--- src/envoy/server/api/depends/csipaus.py
+++ src/envoy/server/api/depends/csipaus.py
@@ -3,6 +3,7 @@
 
 from fastapi import FastAPI
 from starlette.types import Receive, Scope, Send, Message
+from starlette.datastructures import MutableHeaders
 
 
 class CSIPV11aXmlNsOptInMiddleware:
@@ -12,7 +13,11 @@ class CSIPV11aXmlNsOptInMiddleware:
     legacy namespace in the response payload. Note that current thinking is for this is to be a temporary approach in
     support of migration to the new namespace.
 
-    NB. The assumption is we use the V1.1a namespace internally in our validation models.
+    Notes:
+    * The assumption is we use the V1.1a namespace internally in our validation models.
+    * This is a pure ASGIMiddleware as defined here: https://www.starlette.io/middleware/#pure-asgi-middleware.
+    * Full implementation based off brotli middleware package example
+    (https://github.com/fullonic/brotli-asgi/blob/master/brotli_asgi/__init__.py).
     """
 
     equivalent_ns_map: tuple[bytes, bytes] = (b"http://csipaus.org/ns", b"https://csipaus.org/ns")  # (v1.1 , v1.1a)
@@ -23,7 +28,9 @@ class CSIPV11aXmlNsOptInMiddleware:
         Args:
             app (FastAPI): FastAPI app.
         """
-        self.app = app
+        self.app: FastAPI = app
+        self.initial_message: Message = {}
+        self.started: bool = False
 
     def check_opt_in_header(self, scope: Scope) -> bool:
         # Check of v1.1a opt-in header
@@ -36,8 +43,16 @@ class CSIPV11aXmlNsOptInMiddleware:
         """Checks incoming request body for any namespaces in the equivalent_ns_map and replaces them, then on response
         these are mapped back to the original namespace.
         """
-        # Skip running this middleware if opt-in header is not found
-        if scope["type"] != "http" or self.check_opt_in_header(scope):
+
+        # The scope["type"] != "http" is Starlette/asgi kludge or a type defined the asgi spec.
+        # It can be lifespan, http or websocket. We only want to run our middleware on http.
+        # (ref: https://www.starlette.io/middleware/#pure-asgi-middleware)
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        # Skip running this middleware if opt-in header is not found.
+        if self.check_opt_in_header(scope):
             await self.app(scope, receive, send)
             return
 
@@ -50,11 +65,23 @@ class CSIPV11aXmlNsOptInMiddleware:
             return message
 
         async def replace_response_namespace(message: Message) -> None:
-            if message["type"] == "http.response.body":
-                body = message.get("body", False)
+            if message["type"] == "http.response.start":
+                # Don't send the initial message until we've determined how to
+                # modify the outgoing headers correctly.
+                self.initial_message = message
+
+            elif message["type"] == "http.response.body":
+                body = message.get("body", b"")
+
                 if body:
                     body = body.replace(self.equivalent_ns_map[1], self.equivalent_ns_map[0], 1)
                     message["body"] = body
-            await send(message)
+                    headers = MutableHeaders(raw=self.initial_message["headers"])
+                    headers["Content-Length"] = str(len(body))
+
+                if not self.started:
+                    self.started = True
+                    await send(self.initial_message)
+                await send(message)
 
         await self.app(scope, replace_request_namespace, replace_response_namespace)
