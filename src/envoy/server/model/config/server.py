2850ee3fc4e1cbf6d029883db601a7f460d516f6
mhabib <mustafa.habib@anu.edu.au>
2025-05-17 10:37:05 +1000

    wip - tidy

--- src/envoy/server/model/config/server.py
+++ src/envoy/server/model/config/server.py
@@ -1,9 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict, replace
+from typing import Optional
+
+from envoy.server.model import server
 
 
 @dataclass(slots=True, frozen=True)
-class RuntimeServerConfig:
-    """Represents the runtime server configurations."""
+class DynamicServerConfiguration:
+    """Internal Domain model that represents runtime server configurations that can be varied dynamically."""
 
     dcap_pollrate_seconds: int = 300
     edevl_pollrate_seconds: int = 300
@@ -11,3 +14,25 @@ class RuntimeServerConfig:
     derpl_pollrate_seconds: int = 60
     derl_pollrate_seconds: int = 60
     mup_postrate_seconds: int = 60
+
+
+# reference default values
+default = DynamicServerConfiguration()
+
+
+# NOTE: Too simple to have dedicated manager, mapper, etc.
+def map_server_config(
+    live_config: Optional[server.DynamicServerConfiguration] = None,
+) -> DynamicServerConfiguration:
+    if not live_config:
+        return default
+
+    # extract expected fields from domain model
+    cfg_fields = asdict(default).keys()
+
+    live_values = {
+        field: getattr(live_config, field) for field in cfg_fields if getattr(live_config, field) is not None
+    }
+
+    # return new instance with non-null values from entity replacing those in default
+    return replace(default, **live_values)
