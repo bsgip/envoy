db9c7c28a0c5204e6ce45432478e780a06d3468d
must-h92 <mustafa.habib@anu.edu.au>
2025-10-28 16:47:08 +1100

    add int tests using format str literals

--- tests/integration/general/test_endpoint_exclusion.py
+++ tests/integration/general/test_endpoint_exclusion.py
@@ -3,7 +3,7 @@ import urllib
 import pytest
 
 from httpx import AsyncClient
-from envoy_schema.server.schema.uri import TimeUri
+from envoy_schema.server.schema.uri import TimeUri, EndDeviceUri
 
 from envoy.server.endpoint_exclusion import HTTPMethod
 
@@ -22,12 +22,42 @@ async def test_exclude_endpoints_no_method(client: AsyncClient):
     assert resp.status_code == HTTPStatus.METHOD_NOT_ALLOWED
 
 
+@pytest.mark.exclude_endpoints([(HTTPMethod.GET, EndDeviceUri)])
+@pytest.mark.anyio
+async def test_exclude_endpoints_no_method_formattable_uri(client: AsyncClient):
+    """Test basic filter usage where route still exist but a specific method has been removed,
+    using a formattable uri."""
+    # Act
+    resp = await client.get(
+        EndDeviceUri.format(site_id=1),  # site included in base_config.sql
+        headers={cert_header: urllib.parse.quote(TEST_CERTIFICATE_FINGERPRINT)},
+    )
+
+    # Assert
+    assert resp.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+
 @pytest.mark.exclude_endpoints([(HTTPMethod.HEAD, TimeUri), (HTTPMethod.GET, TimeUri)])
 @pytest.mark.anyio
 async def test_exclude_endpoints_no_route(client: AsyncClient):
     """Test where all methods of a route have been removed, expecting the entire route to be removed i.e. NOT FOUND"""
     # Act
     resp = await client.head(TimeUri, headers={cert_header: urllib.parse.quote(TEST_CERTIFICATE_FINGERPRINT)})
+
+    # Assert
+    assert resp.status_code == HTTPStatus.NOT_FOUND
+
+
+@pytest.mark.exclude_endpoints(
+    [(HTTPMethod.HEAD, EndDeviceUri), (HTTPMethod.GET, EndDeviceUri), (HTTPMethod.DELETE, EndDeviceUri)]
+)
+@pytest.mark.anyio
+async def test_exclude_endpoints_no_route_formattable_uri(client: AsyncClient):
+    """Test where all methods of a route have been removed, expecting the entire route to be removed i.e. NOT FOUND"""
+    # Act
+    resp = await client.head(
+        EndDeviceUri.format(site_id=1), headers={cert_header: urllib.parse.quote(TEST_CERTIFICATE_FINGERPRINT)}
+    )
 
     # Assert
     assert resp.status_code == HTTPStatus.NOT_FOUND
