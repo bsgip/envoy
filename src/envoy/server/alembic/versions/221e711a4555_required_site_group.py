7a603d73c7f1b2d610b356aa73788474e784f32c
Josh Vote <jjvote@gmail.com>
2026-07-29 11:49:20 +1000

    Added support for default SiteGroup

--- src/envoy/server/alembic/versions/221e711a4555_required_site_group.py
+++ src/envoy/server/alembic/versions/221e711a4555_required_site_group.py
@@ -17,6 +17,7 @@ depends_on = None
 
 def upgrade() -> None:
     op.add_column("site_control_group", sa.Column("required_site_group_id", sa.Integer(), nullable=True))
+
     op.create_index(
         op.f("ix_site_control_group_required_site_group_id"),
         "site_control_group",
@@ -43,8 +44,17 @@ def upgrade() -> None:
     )
     op.add_column("archive_tariff", sa.Column("required_site_group_id", sa.Integer(), nullable=True))
 
+    op.add_column(
+        "site_group",
+        sa.Column("default_group", sa.BOOLEAN(), nullable=False, server_default="false"),
+    )
+    op.create_index(op.f("ix_site_group_default_group"), "site_group", ["default_group"], unique=False)
+
 
 def downgrade() -> None:
+    op.drop_index(op.f("ix_site_group_default_group"), table_name="site_group")
+    op.drop_column("site_group", "default_group")
+
     op.drop_column("archive_tariff", "required_site_group_id")
     op.drop_constraint("tariff_required_site_group_id_fkey", "tariff", type_="foreignkey")
     op.drop_index(op.f("ix_tariff_required_site_group_id"), table_name="tariff")
--- src/envoy/server/crud/site_group.py
+++ src/envoy/server/crud/site_group.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from sqlalchemy import and_, or_, select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import InstrumentedAttribute
 from sqlalchemy.sql import ColumnElement
 from sqlalchemy.sql.selectable import Exists
 
-from envoy.server.model.site import Site, SiteGroupAssignment
+from envoy.server.model.site import Site, SiteGroup, SiteGroupAssignment
 
 
 def site_is_member_of_group(site_group_id_col: InstrumentedAttribute[int], site_id: int) -> Exists:
@@ -58,3 +61,19 @@ def required_site_group_visible_to_site(
         .exists()
     )
     return or_(required_site_group_id_col.is_(None), member_exists)
+
+
+async def assign_default_site_groups_to_site(session: AsyncSession, site_id: int, changed_time: datetime) -> None:
+    """Adds a SiteGroupAssignment linking site_id to every SiteGroup marked as default_group=True. Intended to be
+    called immediately after a new Site is created so it automatically becomes a member of the "default" groups.
+
+    Does not flush/commit - it's expected the caller will do so as part of the enclosing transaction."""
+
+    default_group_ids = (
+        (await session.execute(select(SiteGroup.site_group_id).where(SiteGroup.default_group.is_(True))))
+        .scalars()
+        .all()
+    )
+
+    for site_group_id in default_group_ids:
+        session.add(SiteGroupAssignment(site_id=site_id, site_group_id=site_group_id, changed_time=changed_time))
